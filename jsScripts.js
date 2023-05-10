5225e918f75e4022b336bb64f3b9efaf8e01976a
Shlomas <shaishlomai@gmail.com>
2023-05-10 16:13:33 +0300

    Add internal page Add link to domain

--- jsScripts.js
+++ jsScripts.js
@@ -2,3 +2,12 @@ function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
 }
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+function sectionToShow(i) {
+  document.getElementById("main.about").style.display= "block";
+}
